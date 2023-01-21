244d41fc90f7cd50688f7522b5626bcdd2d0eff8
Sai Not Siddharth <123229841+Sai-Siddharth619@users.noreply.github.com>
2023-01-21 08:21:23 +0000

    Added functions to the choices

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import random
 wrongpass = 0
+# A variable that stores the number of attempts you entered wrong 
 password = "Oak-cODEfest_2-3"
-passasker = input("Enter the password: ")
+#If you enter this password you can access the game.
+passasker = input("Enter the password for accessing the game ")
+#It is a variable that stores your input of the password.
 if passasker == "Oak-cODEfest_2-3":
   print("Password Accepted")
+#This is an if statement that tells if the variable(passasker) == to the right password.
 
   
 else:
@@ -27,8 +32,12 @@ if wrongpass == 5:
   print("Too many failed attempts :(")
   quit()
 print("\t\t\t\t\tTECHRIDGERS")
-playername = input("What should we call you?: ")
-print("Hello", playername, "Let's play a game!")
+playername = input("What is your name?: ")
+playerage = input("How old are you?:" )
+playerclass = input("What class you are in ")
+
+print("Hello", playername, "! Let's play a game!")
+
 userwins=""
 #userwins is a variable that shows how many wins the user has.
 compwins=""
@@ -36,14 +45,19 @@ compwins=""
 
 print("\n REMEMBER: \n 1. Tech beats Gers \n 2. Gers beats Rid \n 3. Rid beats Tech \n \n Choose wisely and have fun :)")
  
-userchoice=input("Choose Tech, Rid, or Gers: ")
-if userchoice=="Tech" or "tech":
-  user_choice= "Tech"
+userchoice=input("Type-\n 1 for Tech \n 2 for Rid \n 3 for Gers \n Your answer :- ")
+if userchoice=="1":
   print("Your choice is Tech")
-elif userchoice=="Rid" or "rid":
+  user_choice= "Tech"
+elif userchoice=="2":
+  print("Your choice is Rid")
   user_choice= "Rid"
-elif userchoice=="Ger" or "ger":
+elif userchoice=="3":
+  print("Your choice is Ger")
   user_choice="Ger"
-else:
-  print("Not a Valid Choice")
+print("Now for the computers turn\n")
+compchoice=random.randint(1,3)
+if compchoice
+
+
 
