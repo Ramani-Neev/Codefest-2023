efa7ef40a63f8bac626273fa5e902bf41f5e779c
Neev Ramani <93311752+Ramani-Neev@users.noreply.github.com>
2023-01-21 06:16:18 +0000

    Trial

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-import random
-wrongpass = 0
+wrrrongpass = 0
 password = "Oak-cODEfest_2-3"
-passasker = input("Enter the password: ")
+passasker = input("Enter the password")
 if passasker == "Oak-cODEfest_2-3":
   print("Password Accepted")
 else:
   wrongpass = wrongpass + 1
-  print("Access Denied")
   while wrongpass < 5:
-    passasker = input()
+    print("Access Denied")
+    passasker = input("Enter the password: ")
     if passasker == "Oak-cODEfest_2-3":
       print("Password Accepted")
-      
     else:
       wrongpass = wrongpass + 1
       print("Access Denied")
+      
+      passasker = input("Enter the password")
+      if passasker == "Oak-cODEfest_2-3":
+      print("Password Accepted")
+      else:
+      wrongpass = wrongpass + 1
+      print("Access Denied")
+
+
 
 
 print("\t\t\t\t\tTECHRIDGERS")
