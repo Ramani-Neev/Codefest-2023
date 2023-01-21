572e707e221d45d5423d8ae2441f164dc980bf1a
Neev Ramani <>
2023-01-21 05:47:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+wrongpass = 0
+password = "Oak-cODEfest_2-3"
+passasker = input("Enter the password")
+if passasker == "Oak-cODEfest_2-3":
+  print("Password Accepted")
+else:
+  wrongpass = wrongpass + 1
+  while wrongpass < 5:
+    print("Access Denied")
+    wrongpass = wrong pass
+
+
+
+
+print("\t\t\t\t\tTECHRIDGERS")
+playername = input("What should we call you?: ")
+print("Hello", playername)
+userwins=""
+#userwins is a variable that shows how many wins the user has.
+compwins=""
+#compwins is a variable that shows how many wins the user has.
+
+print("\n REMEMBER: \n 1. Tech beats Gers \n 2. Gers beats Rid \n 3. Rid beats Tech \n \n Choose wisely and have fun :)")
