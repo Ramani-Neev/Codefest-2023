fe41ca313fb462ef94d655b92e5d4e01f2e14367
Neev Ramani <93311752+Ramani-Neev@users.noreply.github.com>
2023-01-21 06:19:22 +0000

    Acces

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
-wrrrongpass = 0
+wrongpass = 0
 password = "Oak-cODEfest_2-3"
-passasker = input("Enter the password")
+passasker = input("Enter the password: ")
 if passasker == "Oak-cODEfest_2-3":
   print("Password Accepted")
 else:
   wrongpass = wrongpass + 1
+  print("Access Denied")
   while wrongpass < 5:
-    print("Access Denied")
-    passasker = input("Enter the password: ")
+    passasker = input("Try again: ")
     if passasker == "Oak-cODEfest_2-3":
       print("Password Accepted")
     else:
       wrongpass = wrongpass + 1
       print("Access Denied")
       
-      passasker = input("Enter the password")
+      passasker = input("Try Again: ")
       if passasker == "Oak-cODEfest_2-3":
-      print("Password Accepted")
+        print("Password Accepted")
       else:
-      wrongpass = wrongpass + 1
-      print("Access Denied")
+        wrongpass = wrongpass + 1
+        print("Access Denied")
 
 
 
 
-print("\t\t\t\t\tTECHRIDGERS")
-playername = input("What should we call you?: ")
-print("Hello", playername)
-userwins=""
-#userwins is a variable that shows how many wins the user has.
-compwins=""
-#compwins is a variable that shows how many wins the user has.
 
-print("\n REMEMBER: \n 1. Tech beats Gers \n 2. Gers beats Rid \n 3. Rid beats Tech \n \n Choose wisely and have fun :)")
